f92b10060b7acdf3c925ce5f9af5e4283058a717
adiy55 <adiyafe55@gmail.com>
2023-04-21 20:45:36 +0300

    Update notebook + full run

---
+++ StarMatching.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+
+def calculate_transformation(H, src_pt):
+    x, y = src_pt
+    new_coords = H @ np.array([x, y, 1])
+    new_coords /= new_coords[-1]  # divide by last coordinate to normalize
+    new_coords = new_coords[:-1]  # remove last index
+    if sum(new_coords < 0) == 0:  # check that both x, y coordinates are positive!
+        return new_coords
+    return None
