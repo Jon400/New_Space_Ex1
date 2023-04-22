41e5ef71d5b439dcfafbbedd608b36df96082b68
adiy55 <adiyafe55@gmail.com>
2023-04-23 00:03:35 +0300

    Add Hough Circles again

--- StarDetector.py
+++ StarDetector.py
@@ -14,12 +14,39 @@ def __threshold_image(img, thresh=150, max_value=255):
     return img_bin
 
 
+# https://docs.opencv.org/4.x/dd/d1a/group__imgproc__feature.html#ga47849c3be0d0406ad3ca45db65a25d2d
+# https://docs.opencv.org/4.x/da/d53/tutorial_py_houghcircles.html
+def __get_hough_circles(img):
+    img_bin = __threshold_image(img)
+    img_bin = cv2.GaussianBlur(img_bin, (7, 7), 0)
+    circles = cv2.HoughCircles(img_bin, cv2.HOUGH_GRADIENT, 1, 20,
+                               param1=300, param2=0.8, minRadius=2, maxRadius=4)
+    return circles
+
+
+def find_stars(img, as_pandas=False):
+    coords, kps = [], []
+    circles = __get_hough_circles(img)
+    if circles is not None:
+        for x, y, r in circles[0]:
+            x, y, r = int(x), int(y), round(r, 2)
+            b = img[y, x] / np.max(img)
+            diameter = 2 * r
+            coords.append((x, y, r, b))
+            kps.append(cv2.KeyPoint(x, y, diameter))
+    coords_arr = np.array(coords, dtype=np.uint32) if len(
+        coords) > 0 else coords  # empty np.array raises an error in pd.DataFrame
+    if as_pandas:
+        return kps, pd.DataFrame(coords_arr, columns=['x', 'y', 'r', 'b'], dtype=np.uint32)
+    return kps, coords_arr
+
+
 def __get_blobs(img):
     params = cv2.SimpleBlobDetector_Params()
 
     params.filterByArea = True
     params.minArea = 10
-    params.maxArea = 600
+    params.maxArea = 250
 
     params.filterByCircularity = True
     params.minCircularity = 0.1
@@ -35,7 +62,7 @@ def __get_blobs(img):
     return kp
 
 
-def find_stars(img, as_pandas=False):
+def find_blobs(img, as_pandas=False):
     keypoints = __get_blobs(img)
     coords = []
     for kp in keypoints:
@@ -74,3 +101,9 @@ def plot_detected_stars(img, kps):
 if __name__ == '__main__':
     im1_path = "Ex1_test_101/fr1.jpg"
     keypoints1, im1_data = find_stars(load_image(im1_path))
+
+    im2_path = "Ex1_test_101/ST_db2.png"
+    im2 = load_image(im2_path)
+    keypoints2, im2_data = find_stars(im2)
+
+    plot_detected_stars(im2, keypoints2)
